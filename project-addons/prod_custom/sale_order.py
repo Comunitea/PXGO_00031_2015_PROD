9c266cd960fbbc97233a126202206062314b8ff8
jesusVMayor <jesus@comunitea.com>
2016-09-30 14:33:30 +0200

    [IMP]prod_custom: No se añade el código interno a la descripción de las lineas de venta.

--- project-addons/prod_custom/sale_order.py
+++ project-addons/prod_custom/sale_order.py
@@ -33,3 +33,19 @@ class SaleOrder(models.Model):
             order.signal_workflow('ship_end')
             if order.state != 'done':
                 order.state = 'done'
+
+class SaleOrderLine(models.Model):
+
+    _inherit = 'sale.order.line'
+
+    @api.cr_uid_ids_context
+    def product_id_change(self, cr, uid, ids, pricelist, product, qty=0,
+            uom=False, qty_uos=0, uos=False, name='', partner_id=False,
+            lang=False, update_tax=True, date_order=False, packaging=False,
+            fiscal_position=False, flag=False, context=None):
+        context = dict(context)
+        context['display_default_code'] = False
+        return super(SaleOrderLine, self).product_id_change(
+            cr, uid, ids, pricelist, product, qty, uom, qty_uos, uos, name,
+            partner_id, lang, update_tax, date_order, packaging,
+            fiscal_position, flag, context)
