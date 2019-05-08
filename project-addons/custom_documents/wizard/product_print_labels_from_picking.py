d4b7b5f8d97e62052fcce2a4516412462d3d5a99
JesusVMayor <jesus@comunitea.com>
2019-05-08 16:16:22 +0200

    [IMP]custom_documents: Modificado wizard de albarán para imprimir directamente.

--- project-addons/custom_documents/wizard/product_print_labels_from_picking.py
+++ project-addons/custom_documents/wizard/product_print_labels_from_picking.py
@@ -1,18 +1,30 @@
 # -*- coding: utf-8 -*-
 # © 2017 Comunitea
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-from openerp import models, fields, api, exceptions, _
+from openerp import models, fields, api
 
 
 class ProductPrintLabelsFromPicking(models.TransientModel):
 
     _name = 'product.print.labels.from.picking'
 
+    printer_id = fields.Many2one(
+        comodel_name='printing.printer', string='Printer', required=True,
+        help='Printer used to print the labels.')
+
     @api.multi
     def print_label(self):
         self.ensure_one()
-        datas = {'ids': self.env['stock.picking'].browse(
-            self._context['active_ids']).mapped('pack_operation_ids.id')}
-        return self.env['report'].get_action(
-            self, 'custom_documents.report_product_label_from_picking',
-            data=datas)
+        for pack_operation in self.env['stock.picking'].browse(
+                self._context['active_ids']).mapped('pack_operation_ids'):
+            wizard = self.env['wizard.print.record.label'].create({
+                'printer_id': self.printer_id.id,
+                'label_id':
+                self.env.ref('custom_documents.product_product_zpl_label').id,
+            })
+            location_name = ' / '.join(
+                pack_operation.location_dest_id.complete_name.split(' / ')[1:])
+            wizard.with_context(
+                location_name=location_name,
+                active_ids=[pack_operation.product_id.id],
+                active_model='product.product').print_label()
